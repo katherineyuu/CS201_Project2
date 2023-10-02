44bfe5ec7c5468c1aceb73544614c1112b24685f
Katherine Yu <katherine.l.yu@duke.edu>
2023-10-02 16:48:06 -0400

    trial 1

--- src/HashMarkov.java
+++ src/HashMarkov.java
@@ -1,35 +1,84 @@
-import java.util.List;
+import java.util.*;
 
 public class HashMarkov implements MarkovInterface {
+    private String[] myWords; // Training text split into array of words
+    private Random myRandom; // Random number generator
+    private int myOrder; // Length of WordGrams used
+    protected static String END_OF_TEXT = "*** ERROR ***";
+    private HashMap<WordGram, List<String>> myMap = new HashMap<>();
+
+    public HashMarkov(int order) {
+        myOrder = order;
+        myRandom = new Random();
+        myWords = null;
+    }
+
+    public HashMarkov () {
+		this(3);
+	}
 
     @Override
     public void setTraining(String text) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setTraining'");
+        myWords = text.split("\\s+");
+        if (myMap != null) {
+            myMap.clear();
+        }
+        for (int i = 0; i < myWords.length - myOrder; i++) {
+            WordGram currentWG = new WordGram(myWords, i, myOrder);
+            if (myMap.containsKey(currentWG)) {
+                myMap.get(currentWG).add(myWords[i + myOrder]);
+            } else {
+                myMap.put(currentWG, new ArrayList<>());
+                myMap.get(currentWG).add(myWords[i + myOrder]);
+            }
+        }
     }
 
     @Override
     public List<String> getFollows(WordGram wgram) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getFollows'");
+        List<String> arr = new ArrayList<>();
+        if (!myMap.containsKey(wgram)) {
+            return arr;
+        }
+        arr = myMap.get(wgram);
+        return arr;
     }
 
     @Override
     public String getRandomText(int length) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRandomText'");
-    }
+        ArrayList<String> randomWords = new ArrayList<>(length);
+        int index = myRandom.nextInt(myWords.length - myOrder + 1);
+        WordGram current = new WordGram(myWords, index, myOrder);
+        randomWords.add(current.toString());
 
+        for (int k = 0; k < length - myOrder; k += 1) {
+            String nextWord = getNextWord(current);
+            if (nextWord.equals(END_OF_TEXT)) {
+                break;
+            }
+            randomWords.add(nextWord);
+            current = current.shiftAdd(nextWord);
+        }
+        return String.join(" ", randomWords);
+    }
+    private String getNextWord(WordGram wgram) {
+		List<String> follows = getFollows(wgram);
+		if (follows.size() == 0) {
+			return END_OF_TEXT;
+		}
+		else {
+			int randomIndex = myRandom.nextInt(follows.size());
+			return follows.get(randomIndex);
+		}
+	}
     @Override
     public int getOrder() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOrder'");
+        return myOrder;
     }
 
     @Override
     public void setSeed(long seed) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setSeed'");
+        myRandom.setSeed(seed);
     }
-    
+
 }
--- src/WordGram.java
+++ src/WordGram.java
@@ -3,83 +3,89 @@
  * sequence of words.
  * For use in Compsci 201, Duke University, Fall 2022
  * Add yourself as an author when you make edits
+ * 
  * @author Brandon Fain
  * @author Owen Astrachan, revised Fall 2023
  */
 
 public class WordGram {
-	private String[] myWords; 	// Stores WordGram words
-	private String myToString;	// Stores space separated words as one string
-	private int myHash;			// Stores hash value of WordGram
+	private String[] myWords; // Stores WordGram words
+	private String myToString; // Stores space separated words as one string
+	private int myHash; // Stores hash value of WordGram
 
-	
 	/**
 	 * Constructor should generate a WordGram with size words
 	 * beginning at the start index value of source array.
 	 * Each element of source array should be a single word.
+	 * 
 	 * @param source Source array, each element should be a single word
-	 * @param start Index of first word for WordGram object
-	 * @param size Number of elements in WordGram object
+	 * @param start  Index of first word for WordGram object
+	 * @param size   Number of elements in WordGram object
 	 */
 	public WordGram(String[] source, int start, int size) {
-		// TODO correctly implement constructor
 		myWords = new String[size];
-		for(int k=0; k < size; k++) {
-			myWords[k] = "hello";
+		for (int k = 0; k < size; k++) {
+			myWords[k] = source[start + k];
 		}
+		myToString = null;
+		myHash = 0;
 	}
 
 	/**
 	 * Returns number of words in this WordGram
+	 * 
 	 * @return order of wordgram, number of words
 	 */
 	public int length() {
-		// TODO correctly implement length 
-		return 0;
+		// TODO correctly implement length
+		return myWords.length;
 	}
 
-
-	/** 
+	/**
 	 * Returns true if o is also a wordgram with the
-	 * same words, otherwise returns false 
-	*/
+	 * same words, otherwise returns false
+	 */
 	@Override
 	public boolean equals(Object o) {
-		if (! (o instanceof WordGram) || o == null){
+		if (!(o instanceof WordGram) || o == null) {
 			return false;
 		}
 		WordGram other = (WordGram) o;
-		// TODO complete correct implementation of equals (above is correct)
-		return false;
+		if (this.length() != other.length() ) return false;
+		return this.toString().equals(other.toString());
 	}
 
-
 	/**
-	 * Returns a hashCode for WordGram object equal to 
-	 * the hashCode of the space separated words stored in 
+	 * Returns a hashCode for WordGram object equal to
+	 * the hashCode of the space separated words stored in
 	 * the WordGram.
 	 */
 	@Override
 	public int hashCode() {
-		// TODO correctly implement hashCode
-		return 0;
+		if (myHash == 0) {
+		myHash =this.toString().hashCode();
+	}
+		return myHash;
 	}
 
-
 	/**
-	 * Return a new WordGram of the same length as this WordGram 
+	 * Return a new WordGram of the same length as this WordGram
 	 * consisting of words 1 through length-1 of this WordGram
 	 * followed by last. Does NOT mutate this WordGram.
+	 * 
 	 * @param last added as last string of returned WordGram
-	 * Should be a single word
+	 *             Should be a single word
 	 * @return new WordGram
 	 */
 	public WordGram shiftAdd(String last) {
 		// TODO correctly implement shiftAdd
-		WordGram wg = new WordGram(myWords, 0, length());
+		WordGram wg = new WordGram(myWords, 0, myWords.length);
+		for (int i = 1; i < myWords.length; i++) {
+			wg.myWords[i - 1] = wg.myWords[i];
+		}
+		wg.myWords[myWords.length - 1] = last;
 		return wg;
 	}
-
 
 	/**
 	 * Returns space separated words stored in the WordGram
@@ -88,6 +94,9 @@ public class WordGram {
 	@Override
 	public String toString() {
 		// TODO correctly implement toString
-		return "";
+		if (myToString == null) {
+			myToString = String.join(" ", myWords);
+		}
+		return myToString;
 	}
 }
